d4c06b3336a09eba5ced5d352af016bd5d3fb4a6
avbatchelor <alexbatchelor@hotmail.co.uk>
2018-02-05 14:37:55 -0500

    started to add code to move data from server to lab desktop

--- microCzarSettings.m
+++ microCzarSettings.m
@@ -7,11 +7,13 @@ computer = getComputerID;
 
 if strcmp(computer,'desktop')
     % Backup raw data without removing deleted files from server
-    backupScript{1} = 'robocopy d:\DataD z:\DataD-copy /fft /e';
+    backupScript{1} = 'robocopy d:\otherData\DataD z:\DataD-copy /fft /e';
 
     % Backup processed data and remove deleted files from server - since
     % processed files may be updated 
-    backupScript{2} = 'robocopy d:\ProcessedDataD z:\ProcessedDataD-copy /fft /mir';
+    backupScript{2} = 'robocopy d:\otherData\ProcessedDataD z:\ProcessedDataD-copy /fft /mir';
+    
+    % 
 elseif strcmp(computer,'behavior')
     % Backup raw data without removing deleted files from server
     backupScript{1} = 'robocopy C:\Users\Alex\Documents\Data z:\BehaviorData /fft /e';
