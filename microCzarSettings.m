da1c29147c0b5ff4148a7bd170058ce9808523be
avbatchelor <alexbatchelor@hotmail.co.uk>
2018-02-05 16:32:02 -0500

    changed backup folders + ignoring asv files

--- microCzarSettings.m
+++ microCzarSettings.m
@@ -6,20 +6,24 @@
 computer = getComputerID;
 
 if strcmp(computer,'desktop')
-    % Backup raw data without removing deleted files from server
-    backupScript{1} = 'robocopy d:\otherData\DataD z:\DataD-copy /fft /e';
-
-    % Backup processed data and remove deleted files from server - since
-    % processed files may be updated 
-    backupScript{2} = 'robocopy d:\otherData\ProcessedDataD z:\ProcessedDataD-copy /fft /mir';
+%     % Backup raw data without removing deleted files from server
+%     backupScript{1} = 'robocopy d:\otherData\DataD z:\DataD-copy /fft /e';
+% 
+%     % Backup processed data and remove deleted files from server - since
+%     % processed files may be updated 
+%     backupScript{2} = 'robocopy d:\otherData\ProcessedDataD z:\ProcessedDataD-copy /fft /mir';
     
-    % 
+    % Move behavior data on server to lab desktop
+    backupScript{1} = 'robocopy z:\ManuscriptData\rawData d:\ManuscriptData\rawData /fft /e /NDL /nc /ns /np';
+
+    % Backup processed behavior data on server to lab desktop 
+    backupScript{2} = 'robocopy z:\ManuscriptData\processedData d:\ManuscriptData\processedData /fft /e /NDL /nc /ns /np';
 elseif strcmp(computer,'behavior')
     % Backup raw data without removing deleted files from server
-    backupScript{1} = 'robocopy C:\Users\Alex\Documents\Data z:\BehaviorData /fft /e';
+    backupScript{1} = 'robocopy C:\Users\Alex\Documents\Data z:\ManuscriptData\rawData /fft /e';
 
     % Backup processed data and remove deleted files from server - since
     % processed files may be updated 
-    backupScript{2} = 'robocopy C:\Users\Alex\Documents\ProcessedData z:\ProcessedBehaviorData /fft /mir';
+    backupScript{2} = 'robocopy C:\Users\Alex\Documents\ProcessedData z:\ManuscriptData\processedData /fft /mir';
 end 
 
