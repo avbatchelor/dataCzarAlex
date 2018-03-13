c0da9e985e94db2960ad0af31ce5dd146d8ece69
avbatchelor <alexbatchelor@hotmail.co.uk>
2018-03-13 19:01:21 -0400

    backupg up procesed data between lab desktop, server and laptop@

--- microCzarSettings.m
+++ microCzarSettings.m
@@ -13,17 +13,31 @@ if strcmp(computer,'desktop')
 %     % processed files may be updated 
 %     backupScript{2} = 'robocopy d:\otherData\ProcessedDataD z:\ProcessedDataD-copy /fft /mir';
     
+
+    % /fft = Assume FAT File Times (2-second granularity).
+    % /e Copy subdirectories, including Empty ones.
+    % /NDL No Directory List - don't log directory names.
+    % /nc No Class - don't log file classes.
+    % /ns No Size - don't log file sizes.
+    % /np No Progress - don't display percentage copied.
+    % /mir MIRror a directory tree (equivalent to /E plus /PURGE).
+
     % Move behavior data on server to lab desktop
     backupScript{1} = 'robocopy z:\ManuscriptData\rawData d:\ManuscriptData\rawData /fft /e /NDL /nc /ns /np';
-
-    % Backup processed behavior data on server to lab desktop 
-    backupScript{2} = 'robocopy z:\ManuscriptData\processedData d:\ManuscriptData\processedData /fft /e /NDL /nc /ns /np';
+    
+    % Backup processed behavior data on lab desktop to server
+    backupScript{2} = 'robocopy d:\ManuscriptData\processedData z:\ManuscriptData\processedData /fft /e /NDL /nc /ns /np /xo';
+    
+     % Backup processed behavior data on server to lab desktop 
+    backupScript{3} = 'robocopy z:\ManuscriptData\processedData d:\ManuscriptData\processedData /fft /e /NDL /nc /ns /np /xo';
 elseif strcmp(computer,'behavior')
     % Backup raw data without removing deleted files from server
     backupScript{1} = 'robocopy C:\Users\Alex\Documents\Data\ballData z:\ManuscriptData\rawData /fft /e';
 
     % Backup processed data and remove deleted files from server - since
     % processed files may be updated 
-    backupScript{2} = 'robocopy C:\Users\Alex\Documents\ProcessedData\ballData z:\ManuscriptData\processedData /fft /mir';
+    backupScript{2} = 'robocopy C:\Users\Alex\Documents\ProcessedData\ballData z:\ManuscriptData\processedData /fft';
+elseif strcmp(computer,'laptop')
+    backupScript{1} = 'robocopy z:\ManuscriptData\processedData C:\ManuscriptData\processedData /fft /e /NDL /nc /ns /np /xo';
 end 
 
